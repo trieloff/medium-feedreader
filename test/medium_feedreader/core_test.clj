00983f54af484f57efe25fac6653fa755b840720
Lars Trieloff <lars@trieloff.net>
2017-01-01 17:37:43 +0100

    Removed old Clojure code

--- test/medium_feedreader/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns medium-feedreader.core-test
-  (:require [clojure.test :refer :all]
-            [medium-feedreader.core :refer :all]))
-
-(deftest swagger
-  (is (not (nil? app)))
-  (is (not (nil? (app {:request-method :get, :uri "/swagger.json"}))))
-  (spit "target/swagger.json" (slurp (:body (app {:request-method :get, :uri "/swagger.json"})))))
